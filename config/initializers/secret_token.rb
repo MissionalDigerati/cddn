71765553b112bc5b6c808638b9cb4b2b86b125a6
naysayer <joshuagregoryshuster@gmail.com>
2012-12-18 14:54:54 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cddn::Application.config.secret_token = '1da3cc0374263fd44fb7ad83f466d6c66d975c9c401abb1d9e90e5121d614cd039690a5fab2e0e1e52b0df93678e02c2b2d1c3cc74ac004e10fa1fd3797f26b1'
