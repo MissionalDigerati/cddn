31ebb9b4e58685616b2c0c09a7b368105bd7e924
naysayer <joshuagregoryshuster@gmail.com>
2012-12-18 16:45:47 -0800

    added rspec tests for devise and the current user model.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,4 +3,8 @@
 FactoryGirl.define do
   factory :user do
   end
+  factory :defaulted_user, :parent => :user do
+    email "fakeuser@test.com"
+    password "testing"
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    
+    it "should create a valid contact" do 
+      FactoryGirl.create(:defaulted_user).should be_valid
+    end
+    
+    it "should require an the presence of an email address" do
+      FactoryGirl.build(:defaulted_user, :email => nil).should_not be_valid
+    end
+    
+    it "should require a valid email address" do
+      FactoryGirl.build(:defaulted_user, :email => "fred").should_not be_valid
+    end
+    
+    it "should require a password" do
+      FactoryGirl.build(:defaulted_user, password: nil).should_not be_valid
+    end
+    
+  end
+  
 end
