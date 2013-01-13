fd1f1d8edf40350c55fc6d2fb86eb666220269fe
naysayer <joshuagregoryshuster@gmail.com>
2013-01-13 02:02:04 -0800

    added rspec tests for application helper that returns the users name or other info who created an event.

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -13,5 +13,28 @@ describe ApplicationHelper do
       logged_in_as(user).should == "123fake@emailtest.com"
     end
     
+    
+    it "should return the event creator nickname if available" do
+      user = FactoryGirl.create(:defaulted_user, nickname: "fred")
+      event = FactoryGirl.create(:defaulted_event)
+      attendee = FactoryGirl.create(:defaulted_attendee, user_id: user.id, event_id: event.id, attendee_type: "creator")
+      attendee_creator_name(event).should == "fred"
+    end
+    
+    it "should return the events creator's first name if it is available and nickname is not available" do
+      user = FactoryGirl.create(:defaulted_user, first_name: "rory")
+      event = FactoryGirl.create(:defaulted_event)
+      attendee = FactoryGirl.create(:defaulted_attendee, user_id: user.id, event_id: event.id, attendee_type: "creator")
+      attendee_creator_name(event).should == "rory"
+    end 
+    
+    it "should return the user's email if both the nickname and the first name are unavailable" do
+      user = FactoryGirl.create(:defaulted_user, email: "fake@fake.com")
+      event = FactoryGirl.create(:defaulted_event)
+      attendee = FactoryGirl.create(:defaulted_attendee, user_id: user.id, event_id: event.id, attendee_type: "creator")
+      attendee_creator_name(event).should == "fake@fake.com"
+    end
+    
   end
+  
 end
