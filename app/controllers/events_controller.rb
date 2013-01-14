bde33ce7814d999e3f2680af94f67a36bc01f277
naysayer <joshuagregoryshuster@gmail.com>
2013-01-14 13:02:49 -0800

    added event edit and delete actions for users. Moving on to tests next

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -28,6 +28,34 @@ class EventsController < ApplicationController
     end
   end
   
+  def edit
+    @event = Event.find(params[:id])
+  end
+  
+  def update
+    @event = Event.find(params[:id])
+    respond_to do |format|
+      if @event.update_attributes(params[:event])
+        format.html {redirect_to my_events_event_path(current_user)} 
+        flash[:notice] = "Your event was successfully updated."
+      else
+        format.html { render action: "edit" }
+        flash[:notice] = @event.errors.full_messages.to_sentence
+      end
+    end
+  end
+  
+  def destroy
+    @event = Event.find(params[:id])
+    @attendee = Attendee.where("user_id = #{current_user.id} AND event_id = #{@event.id}").first
+    @event.delete
+    @attendee.delete
+    respond_to do |format|
+      format.html {redirect_to my_events_event_path(current_user)}
+      flash[:notice] = "Your event was successfully deleted."
+    end
+  end
+  
   def my_events
     @current_user_attendee_creator = Attendee.where("user_id = #{current_user.id} AND attendee_type = 'creator'")
   end
