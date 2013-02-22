9f2b079b96afa5195ccc79b2e419812c7b43e59b
naysayer <joshuagregoryshuster@gmail.com>
2013-02-21 22:19:30 -0800

    added 5 upcoming events to the home page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   # This is for the home page
   def home
     @projects = Project.approved_projects.limit(5).order("created_at DESC")
+    @events = Event.approved_events.include_date.upcoming_events.limit(5).order_by_date
   end
 end
