0dfa16dfc7f82a824c42b5d392dedf7ccb2fddfb
naysayer <joshuagregoryshuster@gmail.com>
2013-02-24 18:08:43 -0800

    modified button to ensure that it will not display on past events.

--- spec/helpers/events_helper_spec.rb
+++ spec/helpers/events_helper_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 describe EventsHelper do
   describe "methods" do
     
-    it "should return the appropriate button to either attend, unattend an event. Or noting at all if you created the event" do
+    it "should return the appropriate button to either attend, unattend an event. Or noting at all if you created the event and it should only display if it is an upcoming event" do
       user_that_created_event = FactoryGirl.create(:defaulted_user, nickname: "The Master", email: "creator@fake.com")
       event = FactoryGirl.create(:defaulted_event)
+      event.event_dates.create(date_of_event: Time.now.to_date, time_of_event: Time.now)
       attendee = FactoryGirl.create(:defaulted_attendee, user_id: user_that_created_event.id, event_id: event.id, attendee_type: "creator")
       #if the user created the event, no button should display
       attend_event_button(user_that_created_event.id, event).should == ""
@@ -18,6 +19,13 @@ describe EventsHelper do
       user_that_not_attend_event = FactoryGirl.create(:defaulted_user, nickname: "name", email: "name@fake.com")
       #this user will recieve an attend button becuase the are not already attending the event
       attend_event_button(user_that_not_attend_event.id, event).should == "<a href=\"/events/1/attend_event\" class=\"btn btn-mini\" data-method=\"put\" rel=\"nofollow\">Attend Event</a>"
+      
+      #it should recieve nil if the event has passed, nil should be returned for everyone
+      past_event = FactoryGirl.create(:defaulted_event)
+      past_event.event_dates.create(date_of_event: 1.week.ago.to_date, time_of_event: Time.now)
+      attend_event_button(user_that_created_event.id, past_event).should == nil
+      attend_event_button(user_that_attend_event.id, past_event).should == nil
+      attend_event_button(user_that_not_attend_event.id, past_event).should == nil
     end
     
     it "should return a link to the event, unless the event is not approved, in which case it will return nothing" do
